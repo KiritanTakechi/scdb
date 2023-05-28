8f578cea0562aaf395f792a809d1b3cc252a01be
Kiritan <o1090549930o@outlook.com>
2023-05-28 23:34:09 +0800

    前端阶段初步完成

--- src/components/CourseList.vue
+++ src/components/CourseList.vue
@@ -1,11 +1,34 @@
 <template>
-    <h1>课程</h1>
+    <div class="course-list">
+        <el-table height="100%" width="100%" stripe :data="tableData">
+            <el-table-column prop="cno" label="课程号" />
+            <el-table-column prop="name" label="课程名" />
+            <el-table-column prop="credit" label="学分" />
+            <el-table-column prop="semester" label="所在学期" />
+        </el-table>
+    </div>
 </template>
     
 <script setup lang='ts'>
-    
+const tableData = [
+    {
+        cno: 1,
+        name: '数据库',
+        credit: 3,
+        semester: 2,
+    },
+]
 </script>
     
 <style scoped>
-    
+.course-list {
+    height: 100%;
+    width: 100%;
+
+    background-color: whitesmoke;
+
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+}
 </style>
--- src/components/StudentList.vue
+++ src/components/StudentList.vue
@@ -1,11 +1,40 @@
 <template>
-    <h1>学生</h1>
+    <div class="student-list">
+        <el-table height="100%" width="100%" stripe :data="tableData">
+            <el-table-column prop="sno" label="学号" />
+            <el-table-column prop="name" label="姓名" />
+            <el-table-column prop="sex" label="性别" />
+            <el-table-column prop="age" label="年龄" />
+            <el-table-column prop="dept" label="学院" />
+        </el-table>
+    </div>
 </template>
     
 <script setup lang='ts'>
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/tauri";
+
+const tableData = [
+    {
+        sno: 1,
+        name: '丁真',
+        sex: '男',
+        age: 20,
+        dept: '理塘学院',
+    },
+]
 
 </script>
     
 <style scoped>
+.student-list {
+    height: 100%;
+    width: 100%;
 
+    background-color: whitesmoke;
+
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+}
 </style>
